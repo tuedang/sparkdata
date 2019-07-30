12794501bc0c3f4272b005dbd6a39cd105d1a7d5
Tue Dang <dangtritue@gmail.com>
2019-07-30 23:40:43 +0700

    separate queryhelper

---
+++ spring-spark-data/src/main/java/com/tue/service/ElasticQueryHelper.java
@@ -0,0 +1,28 @@
+package com.tue.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.SparkSession;
+import org.elasticsearch.hadoop.rest.query.QueryBuilder;
+import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
+
+public final class ElasticQueryHelper {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    public static <T> Dataset<T> queryForDataSet(JavaSparkContext javaSparkContext, SparkSession sparkSession,
+                                                 String resource, QueryBuilder queryBuilder, Class<T> clazz) {
+        JavaRDD<T> companyRDD = queryForRDD(javaSparkContext, resource, queryBuilder, clazz);
+        return sparkSession.createDataset(companyRDD.rdd(), Encoders.bean(clazz));
+    }
+
+    public static <T> JavaRDD<T> queryForRDD(JavaSparkContext javaSparkContext,
+                                             String resource, QueryBuilder queryBuilder, Class<T> clazz) {
+        JavaPairRDD<String, T> esRdd = JavaEsSpark.esJsonRDD(javaSparkContext, resource, queryBuilder.toString())
+                .mapValues(objectMap -> OBJECT_MAPPER.readValue(objectMap, clazz));
+        return esRdd.map(x -> x._2);
+    }
+}
--- spring-spark-data/src/main/java/com/tue/service/ElasticsearchService.java
+++ spring-spark-data/src/main/java/com/tue/service/ElasticsearchService.java
@@ -1,58 +1,44 @@
 package com.tue.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.elasticsearch.hadoop.rest.query.QueryBuilder;
-import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class ElasticsearchService {
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-
     @Autowired
     private JavaSparkContext sc;
     @Autowired
     private SparkSession sparkSession;
 
     public String handleES(String query) {
-        Dataset<Company> companyDataset = queryForCompany(sc, "vnf/companies",
+        Dataset<Company> companyDataset = ElasticQueryHelper.queryForDataSet(sc, sparkSession, "vnf/companies",
                 CompanyQuery.builder()
                         .withQuery("website:*")
                         .withTerm("name", "tnhh")
-                        .build());
+                        .build(), Company.class);
         companyDataset.show();
 
-        Dataset<Company> companyDatasetVtown = queryForCompany(sc, "vtown*/companies",
+        Dataset<Company> companyDatasetVtown = ElasticQueryHelper.queryForDataSet(sc, sparkSession, "vtown*/companies",
                 CompanyQuery.builder()
                         .withQuery("website:*")
-                        .build());
+                        .build(), Company.class);
         companyDatasetVtown.show();
 
         Dataset<Row> companyJoined = companyDataset.join(companyDatasetVtown,
                 companyDataset.col("website").equalTo(companyDatasetVtown.col("website")));
         companyJoined
                 .select(companyDataset.col("name"),
+                        companyDataset.col("address.province"),
                         companyDatasetVtown.col("name"),
                         companyDataset.col("website"))
-                .show(false);
+                .show(1000, false);
 
         return String.format("{\"count\": \"%s-%s => %s\"}", companyDataset.count(), companyDatasetVtown.count(), companyJoined.count());
-    }
-
-    private Dataset<Company> queryForCompany(JavaSparkContext javaSparkContext, String resource, QueryBuilder queryBuilder) {
-        JavaPairRDD<String, Company> esCompanyRdd = JavaEsSpark.esJsonRDD(javaSparkContext, resource, queryBuilder.toString())
-                .mapValues(objectMap -> OBJECT_MAPPER.readValue(objectMap, Company.class));
-        JavaRDD<Company> companyRDD = esCompanyRdd.map(x -> x._2);
-        return sparkSession.createDataset(companyRDD.rdd(), Encoders.bean(Company.class));
     }
 }
