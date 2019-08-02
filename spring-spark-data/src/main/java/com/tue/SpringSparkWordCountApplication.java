3e9b4eb73b8545dc3374959578beedbe534b51c6
tue.dang <dangtritue@gmail.com>
2019-08-02 18:32:40 +0700

    springboot ui support

--- spring-spark-data/src/main/java/com/tue/SpringSparkWordCountApplication.java
+++ spring-spark-data/src/main/java/com/tue/SpringSparkWordCountApplication.java
@@ -1,12 +1,26 @@
 package com.tue;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.Instant;
 
 @SpringBootApplication
+@EnableAdminServer
+@EnableScheduling
+@Slf4j
 public class SpringSparkWordCountApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSparkWordCountApplication.class, args);
+    }
+
+    @Scheduled(initialDelay = 1000, fixedDelay = 5000)
+    public void processText() {
+        log.info("Execute by cron job {}", Instant.now());
     }
 
 }
